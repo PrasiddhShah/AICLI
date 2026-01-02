798d374854b66cce562affc608cb21028dd929b7
PrasiddhShah <prasiddhshah.18@gmail.com>
2026-01-01 19:37:38 -0800

    token usage added

--- main.py
+++ main.py
@@ -12,4 +12,8 @@ if api_key is None:
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(model="gemini-2.5-flash",contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+if response.usage_metadata is None:
+    raise RuntimeError("Metadata Empty!!!! Please check API Key")
+
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
 print(response.text)
